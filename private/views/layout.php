15c54cdb74e7d72e62b5b8f4c38cdb69e555c2cf
Michael Linhardt <mlinhard@student.42.fr>
2016-11-13 21:23:48 +0100

    remove embbed jquery and add external jquery and bootstrap to layout

--- private/views/layout.php
+++ private/views/layout.php
@@ -7,9 +7,24 @@
 		<link rel="shortcut icon" href="<?php echo IMG_HTTP; ?>favicon.png" type="image/x-icon" />
 		<link rev="start" href="<?php echo ROOT_HTTP; ?>" title="Accueil" />
 		<link rel="stylesheet" type="text/css" href="<?php echo CSS_HTTP; ?>defaut.css">
+		<!-- JQUERY START -->
+		<link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+		<script src="http://code.jquery.com/jquery-3.1.1.min.js"
+			integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+			crossorigin="anonymous"></script>
+		<script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+			integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+			crossorigin="anonymous"></script>
+		<!-- JQUERY END -->
 		<link rel="stylesheet" type="text/css" href="<?php echo CSS_HTTP; ?>jquery.css">
 		<script type="text/javascript" src="<?php echo JS_HTTP; ?>jquery/jquery.js"></script>
 		<script type="text/javascript" src="<?php echo JS_HTTP; ?>jquery/jqueryui.js"></script>
+		<!-- JQUERY END -->
+		<!-- BOOSTRAP START -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- BOOTSTRAP END -->
 		<script type="text/javascript" src="<?php echo JS_HTTP; ?>dump.js"></script>
 		<?php $this->head(); ?>
 		<script type="text/javascript">$(document).ready(function(){  });</script>
